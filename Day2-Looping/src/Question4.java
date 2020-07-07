890f7d380da097d81b6a933195403183030fcbf0
lakshmidevi11 <60636584+lakshmidevi11@users.noreply.github.com>
2020-07-07 22:18:46 +0530

    Add files via upload

---
+++ Day2-Looping/src/Question4.java
@@ -0,0 +1,12 @@
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=100;i>=0;i--) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
---
+++ Day2-Looping/src/Question5.java
@@ -0,0 +1,12 @@
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=100;i>=0;i=i-3) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
